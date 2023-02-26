c36c0f635d01a1bb2d5eaaa0cdb62f96f553205f
Sheldon <sheldonchettiar3@gmail.com>
2023-02-26 17:42:24 +0530

    delete message

--- base/views.py
+++ base/views.py
@@ -94,6 +94,7 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -101,8 +102,15 @@ def room(request, pk):
             room = room,
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room', pk=room.id)
-    context = {'room': room, 'room_messages': room_messages}
+    
+    
+    context = {
+        'room': room, 
+        'room_messages': room_messages,
+        'participants': participants
+        }
     return render(request, 'base/room.html', context)   
 
 @login_required(login_url = 'login')
@@ -141,9 +149,23 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
 
     if request.user != room.host:
-        return HttpResponse("You are not allowed to update!")
+        return HttpResponse("You are not allowed to delete!")
 
     if request.method == 'POST':
         room.delete()
         return redirect("home")
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url = 'login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse("You are not allowed to delete!")
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    return render(request, 'base/delete.html', {'obj': message})
