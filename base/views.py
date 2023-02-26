b2fb78e54bea176346af23664426d6ea6105191f
Sheldon <sheldonchettiar3@gmail.com>
2023-02-26 17:58:02 +0530

    improved search

--- base/views.py
+++ base/views.py
@@ -71,11 +71,14 @@ def registerPage(request):
 
 def home(request): 
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
 
+        Q(host__username__icontains=q) |
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
-        Q(description__icontains=q) 
+        Q(description__icontains=q)
+        
 
         )
 
